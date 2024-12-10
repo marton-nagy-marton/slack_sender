428b9fae8167c4e0a5e87cd63abcffd39eeb6702
Márton Nagy <52880735+marton-nagy-marton@users.noreply.github.com>
2024-12-10 17:09:30 +0100

    Update sender.py

--- sender.py
+++ sender.py
@@ -13,7 +13,7 @@ json_data = {
 }
 
 response = requests.post(
-    f'https://hooks.slack.com/services/T083FD52J10/B083J1S1YMS/{os.environ.get('SLACK_ID')}',
+    f"https://hooks.slack.com/services/T083FD52J10/B083J1S1YMS/{os.environ.get('SLACK_ID')}",
     headers=headers,
     json=json_data,
 )
