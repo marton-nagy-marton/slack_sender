9aa9cfef5d9c47c22d630665b790ab1bab7220f7
Márton Nagy <52880735+marton-nagy-marton@users.noreply.github.com>
2024-12-10 16:57:11 +0100

    Create sender.py

---
+++ sender.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+import requests
+
+headers = {
+    'Content-type': 'application/json',
+}
+
+json_data = {
+    'text': 'Hello, slack!',
+}
+
+response = requests.post(
+    f'https://hooks.slack.com/services/T083FD52J10/B083J1S1YMS/{os.environ.get('SLACK_ID')}',
+    headers=headers,
+    json=json_data,
+)
+
+print(response)
